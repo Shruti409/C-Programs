e48d89a314b181d5e55c63e6ee5fa232c6b2ab62
Shruti409 <60936697+Shruti409@users.noreply.github.com>
2021-07-19 21:41:51 +0530

    Add files via upload

---
+++ basics/fahrenheit_to_celcius.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float fahrenheit,celcius;
+    cout<<"Enter temperature in fahrenheit "<<endl;
+    cin>>fahrenheit;
+    celcius=5.0/9.0*(fahrenheit-32);
+    cout<<"Temperature in celcius is:"<<celcius;
+    
+    
+
+
+    return 0;
+
+}
